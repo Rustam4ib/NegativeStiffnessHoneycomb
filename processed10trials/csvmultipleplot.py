363d1a58d806d7c27f05ae89e59a95afb79fc76b
Rustam4ib <https://Rustam4ib@github.com>
2022-02-10 20:26:07 +0600

    3rd commit

--- processed10trials/csvmultipleplot.py
+++ processed10trials/csvmultipleplot.py
@@ -1,104 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
 
-#plt.rcParams["figure.figsize"] = [7.50, 3.50]
-#plt.rcParams["figure.autolayout"] = True
+path = "/home/rustam/catkin_ws/src/calibrate_rgb_sensor_rustam/src/processed10trials" # use your path
+all_files = glob.glob(path + "/*.csv")
 
-# headers = ['fz', 'z']
-# df5 = pd.read_csv('/home/rustam/catkin_ws/src/calibrate_rgb_sensor_rustam/src/rosbag_data/newpc_myrosbag_ur5_and_weiss_file_5try/new.csv', names=headers)
-# df6 = pd.read_csv('/home/rustam/catkin_ws/src/calibrate_rgb_sensor_rustam/src/rosbag_data/newpc_myrosbag_ur5_and_weiss_file_6try/new.csv', names=headers)
-# #print (df.dtypes)
-# x5 = df5['z']
-# y5 = df5['fz']
-# y5 = pd.to_numeric(df5['fz'],errors = 'coerce')
-# x5 = pd.to_numeric(df5['z'],errors = 'coerce')
+li = []
+x = []
+y = []
 
-# x6 = df6['z']
-# y6 = df6['fz']
-# y6 = pd.to_numeric(df5['fz'],errors = 'coerce')
-# x6 = pd.to_numeric(df5['z'],errors = 'coerce')
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    x = df['z']
+    y = df['fz']
+    y = pd.to_numeric(df['fz'],errors = 'ignore')
+    x = pd.to_numeric(df['z'],errors = 'ignore')
+    y = y*(-1)
+    x = x.sort_values(ascending=True)
+    x = x*100
+    plt.plot(x,y)
+    #li.append(df)
 
-# y5 = y5*(-1)
-# x5 = x5.sort_values(ascending=True)
-# x5 = x5*100
-# y6 = y6*(-1)
-# x6 = x6.sort_values(ascending=True)
-# x6 = x6*100
-# #print(x)
-
-# plt.xlabel('position (cm)')
-# plt.ylabel('force (N)')
-# plt.plot(x5,y5)
-# plt.plot(x6,y6)
-# plt.grid(True)
-# plt.show()
-
-numFiles = 11 #Number of CSV files in your directory
-separator = "," #Character that separates each value inside file
-fExtension = ".csv" #Extension of the file storing the data
-
-def MultiplePlots(xValues, allYValues):
-    'Method to plot multiple times in one figure.'
-
-    for yValues in allYValues:
-        plt.plot(list(map(int, xValues)), list( map(float, yValues) ), label = "file" + str(i))
-
-    plt.legend(loc = 'best')
-    plt.show()
-    return
-
-def GetXandYValues(coordinates):
-    'Method to get all coordinates from all CSV files.'
-    xValues = []
-    yValues = []
-    allYValues = []
-    fst = False
-    for file in coordinates:
-        for coordinate in file:
-            if (fst == False):
-                xValues.append(coordinate[0])
-            yValues.append(coordinate[1])
-        fst = True
-        allYValues.append( yValues )
-        yValues = []
-    return xValues, allYValues
-
-def GetCoordinates( n , separator , fExtension ):
-    'Iterates through multiple CSV files and storing X values and Y values in different Lists'
-    coordinates = [] #coordinates[0] = x values --- coordinates[1] = y values
-    for i in range(n):
-        coordinates.append( FillList( ReadFile("file" + str(i+1) + fExtension), separator ) )
-    return coordinates
-
-def ReadFile(path):
-    'Function to read CSV file and store file data rows in list.'
-    try:
-        fileCSV = open(path,"r") #Opens file
-        data = fileCSV.read() #Save file data in string
-        listData = data.splitlines() #Split lines so you have List of all lines in file
-        fileCSV.close() #Close file
-    finally:
-        return listData #Return list with file's rows
-
-def FillList(myList, separator):
-    'With this method you make a list containing every row from CSV file'
-    valueTemp = ""
-    listTemp = []
-    newList = []
-    for line in myList:
-        for c in line:
-            if c != separator:
-                valueTemp += c
-            else:
-                listTemp.append( valueTemp )
-                valueTemp = ""
-        listTemp.append( valueTemp )
-        newList.append(listTemp[:])
-        valueTemp = ""
-        del listTemp[:]
-    return newList
-
-xValues = GetXandYValues( GetCoordinates( numFiles, separator , fExtension) )[0]
-allYValues = GetXandYValues( GetCoordinates( numFiles, separator , fExtension) )[1]
-
-MultiplePlots( xValues, allYValues )
+# frame = pd.concat(li, axis=0, ignore_index=True)
+# print(frame)
+# x = frame['z']
+# y = frame['fz']
+# y = pd.to_numeric(df['fz'],errors = 'ignore')
+# x = pd.to_numeric(df['z'],errors = 'ignore')
+# y = y*(-1)
+# x = x.sort_values(ascending=True)
+# x = x*100
+plt.xlabel('position (cm)')
+plt.ylabel('force (N)')
+# plt.plot(x,y)
+plt.grid(True)
+plt.show()
+print("Success")
