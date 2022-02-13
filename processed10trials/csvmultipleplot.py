0478877f171cb4579800c66c6b540fafce2d7e48
Rustam4ib <https://Rustam4ib@github.com>
2022-02-13 22:00:08 +0600

    5th commit(13/02/22):mean

--- processed10trials/csvmultipleplot.py
+++ processed10trials/csvmultipleplot.py
@@ -1,12 +1,15 @@
+from operator import le
 from sqlite3 import Row
 import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import os
 import re
+import numpy as np
 
 mypath = "/home/rustam/catkin_ws/src/calibrate_rgb_sensor_rustam/src/processed10trials" # use your path
-
+li_y = []
+li_x = []
 #The numericalSort() function splits out any digits in a filename, turns it into an actual number
 numbers = re.compile("(\d+)")
 def numericalSort(value):
@@ -15,9 +18,9 @@ def numericalSort(value):
     return parts
 
 def graphdata(path):
-    li = []
-    x = []
-    y = []
+
+    x = [62]
+    y = [62]
     all_files = sorted(glob.glob(path + "/*.csv"), key=numericalSort)
     for filename in all_files:
         df = pd.read_csv(filename, index_col=False, header=0)
@@ -25,38 +28,70 @@ def graphdata(path):
         x = df['z']
         x = pd.to_numeric(df['z'],errors = 'ignore')
         x = x.sort_values(ascending=True)
+        #x = x[:-1]
         x = x*100
+
+        x = x.loc[x>5.02]
+        x = x.loc[x<6.9]
+
+        x = x[0:61]
+        #xnew = pd.Series(x)
         #Set y axis
         y = df['fz']
-        li.append(y)
         y = pd.to_numeric(df['fz'],errors = 'ignore')
         y = y*(-1)
-    return x,y
+        y = y[0:61]
+        li_x.append(x)
+        li_y.append(y)
+        plt.plot(x,y, alpha = 0.2)
+        #print(x.shape)
+        #print(y.shape)
+        #mean_x = x.mean()
+        #mean_y = y.mean()
+        #print(mean_x)
+        #print(mean_y)        
+        #plt.plot(x,mean_y, alpha = 1.0)
+
+    plt.xlabel('position (cm)')
+    plt.ylabel('force (N)')
+    plt.grid(True)
+    #plt.show()
 
 def processedData():
     row_num = []
-    for i in range(len(li)):
-            print(len(li[i]))
-            row_num.append(len(li[i]))
+    for i in range(len(li_x)):
+            print("LEN:"+str(len(li_x[i])))
+            #print(li_x[i])
+            #row_num.append(len(li_x[i]))
     print("min:"+str(min(row_num)))        
     print("Success")
 
 
-def showGraph():
-    x,y = graphdata(mypath)
-    plt.plot(x,y)
-    plt.xlabel('position (cm)')
-    plt.ylabel('force (N)')
-    plt.grid(True)
-    plt.show()
 
-
+def mean_std():
+    row_num = []
+    mean_list = []
+    mean = 0.000
+    li_ynp = np.array(li_y)
+    print("\n")
+    print(li_y)
+    print("\n")
+    for j in range(61):
+        for i in range(11):
+            mean += li_ynp[i][j]
+        mean /= 11    
+        mean_list.append(mean)
+    plt.plot(li_x[0],mean_list, alpha = 1.0)
+    #plt.show()
 
 def main():
+    print("Hello World!")
     graphdata(mypath)
     #processedData()
-    showGraph()
-    print("Hello World!")
+    #showGraph()
+    mean_std()
+    plt.show()
+    print("Bye World!")
 
 if __name__ == "__main__":
     main()
